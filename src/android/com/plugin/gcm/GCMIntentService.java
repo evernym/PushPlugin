61466ad7565aec70f55ca6e81f89934a1d638429
Vivek Tiwari <vivektiwari1986@gmail.com>
2015-05-21 23:18:56 +0530

    Revert "set better summary text"
    This reverts commit 96804da0d93a092409bc0540751c080e7755d0fd.

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -11,7 +11,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -212,7 +211,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 			}
 			Integer otherMessages = xMessages - addedLines;
 			if(otherMessages > 0){
-				inboxStyleNotif.setSummaryText("+ "+otherMessages+" more messages");
+				inboxStyleNotif.setSummaryText("+ "+otherMessages+" more");
 			}			
 		}
 		
